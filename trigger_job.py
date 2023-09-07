2c0b9c24ef3531f1d660032ab0c5bac25b631252
whibbard <whibbard@genies.com>
2023-09-07 18:35:34 -0400

    wip

--- trigger_job.py
+++ trigger_job.py
@@ -1,21 +1,55 @@
 #!/usr/bin/env python3
-# import requests
+'''
+This script does all the operations needed to trigger a workflow job in our github org.  It is dependent on the environment variable 
+GH_JOB_ID being set with the ID value of the Github actions job you are intending for this to trigger.
+The idea here is that you can then deploy this code into more than one lambda in order to be targeting more than one job.
+That means a lambda (which just executes this Python script) has a 1:1 relationship with a single Actions job.
+You can configure the payload to meet whatever inputs you have setup for the receiving Actions job.
+
+'''
 import jwt
 import time
 import sys
 import json
 import urllib.request
+from os import environ
+import boto3
+from botocore.exceptions import ClientError
+# from cryptography.hazmat.backends import default_backend
+# from cryptography.hazmat.primitives import serialization
+
+
+def get_pem():
+
+    secret_name = "github/workflows"
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+
+    secret = get_secret_value_response['SecretString']
+    return secret
 
 
 def executeJob(token, filename, jobid):
-    # params = sys.argv[1]  # one liner null or empty string if nothing
-
     headers = {
         'X-GitHub-Api-Version': '2022-11-28',
         'Accept': 'application/vnd.github+json',
         'Authorization': f'Bearer {token}'
     }
-    payload = {"ref": "main", "inputs": {"artifact":f'{filename}'}} #breaks if you give unexpected inputs? "name": "William", "home": "Knoxville, TN"
+    payload = {"ref": "main", "inputs": {"artifact":f'{filename}'}}
     url = f'https://api.github.com/repos/geniesinc/runner-poc/actions/workflows/{jobid}/dispatches'
 
     req = urllib.request.Request(url, headers=headers, data=json.dumps(payload).encode('utf-8'), method='POST')
@@ -25,7 +59,6 @@ def executeJob(token, filename, jobid):
     except urllib.error.HTTPError as e:
         print(str(e.code))
         print(e.read())
-
 
 def getInstallationToken(jwt_token, githubAppNumber):
     headers = {
@@ -40,15 +73,12 @@ def getInstallationToken(jwt_token, githubAppNumber):
         respJson = json.loads(response.read().decode('utf-8'))
         return respJson['token']
 
-
-
-
-
-
 def getJWT():
-
+    # pem = get_pem() # this is a todo, because I cannot figure out how to get the key to work when coming from AWS secrets.  Will just use file for now.
     pem = "./genies-art-runners.2023-07-17.private-key.pem"
-    app_id = "360400"
+    app_id = "360400" #this is not the same as the app number.
+
+    # pem_bytes = serialization.load_pem_public_key(pem, backend=default_backend())
 
     # Open PEM
     with open(pem, 'rb') as pem_file:
@@ -63,31 +93,25 @@ def getJWT():
         'iss': app_id
     }
 
-    # Create JWT
     jwt_instance = jwt.JWT()
     encoded_jwt = jwt_instance.encode(payload, signing_key, alg='RS256')
 
     return encoded_jwt
 
-
-
-def lambda_handler(event, context):
-    # Parse the S3 event data
-    print("Received event: " + json.dumps(event, indent=2))
-    
-    # Extract the bucket name and object key
+def lambda_handler(event, context):  
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     object_key = event["Records"][0]["s3"]["object"]["key"]
     
-    # Extract the file name from the object key
     filename = object_key.split("/")[-1]
     
-    # Print the file info
     print(f"Object key: {object_key}")
     
-    jobid = "62953910"
+    jobid = environ.get('GH_JOB_ID')
+    if not jobid:
+        raise ValueError("GH_JOB_ID environment variable is not set")
 
-    githubAppNumber = "39576324"
+    githubAppNumber = "39576324" 
+    #number of the github app in our org for this purpose.  This is not the same as the app ID.
 
     jwt = getJWT()
 
@@ -99,37 +123,3 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": "Lambda function executed successfully"
     }
-
-
-# if __name__ == '__main__':
-
-#     jobid = "62953910"
-
-#     githubAppNumber = "39576324"
-
-#     jwt = getJWT()
-
-#     token = getInstallationToken(jwt)
-
-#     executeJob(token)
-
-
-
-
-#if we want to use the requests library, uncomment these.
-#We currently are using all python native libraries in this file.
-# def executeJob(token):
-#     params = sys.argv[1] #one liner null or empty string if nothing
-
-#     headers = {'X-GitHub-Api-Version': '2022-11-28','Accept': 'application/vnd.github+json','Authorization':f'Bearer {token}'}
-#     payload = {"ref":"main","inputs":{"name":"William","home":"Knoxville, TN"}}
-#     r = requests.post(f'https://api.github.com/repos/geniesinc/runner-poc/actions/workflows/{jobid}/dispatches', headers=headers, data=json.dumps(payload))
-#     print(str(r.status_code))
-
-# def getInstallationToken(jwt):
-
-#     headers = {'X-GitHub-Api-Version': '2022-11-28','Accept': 'application/vnd.github+json','Authorization':f'Bearer {jwt}'}
-#     r = requests.post(f'https://api.github.com/app/installations/{githubAppNumber}/access_tokens', headers=headers) 
-#     respJson = r.json()
-
-#     return respJson.token
